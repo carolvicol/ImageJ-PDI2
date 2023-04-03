bceaac92eb5cd9e465fdbac961c06441d5fd0e46
carolvicol <cevillegas@pol.una.py>
2023-04-03 19:09:11 -0400

    Mi primer codigo commiteado

---
+++ src/py/com/InvertirImagen.java
@@ -0,0 +1,10 @@
+package py.com;
+
+public class InvertirImagen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenidos a PDI II");
+	}
+
+}
