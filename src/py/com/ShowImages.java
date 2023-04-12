994d149a7c312833bef2540ac196ad9dc9599aeb
carolvicol <cevillegas@pol.una.py>
2023-04-12 09:33:23 -0400

    Prueba de Comiteado

--- src/py/com/ShowImages.java
+++ src/py/com/ShowImages.java
@@ -2,20 +2,21 @@ package py.com;
 import ij.ImagePlus;
 import ij.process.ImageProcessor;
 
-public class Histograma {
+public class ShowImages {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ImagePlus im= new ImagePlus("C:\\Users\\Familia Villegas\\Desktop\\2023\\DOCTORADO\\PDI 2\\Clases\\cameraman.tif");
+		ImagePlus im= new ImagePlus("C:\\Users\\Familia Villegas\\Desktop\\2023\\DOCTORADO\\PDI 2\\Clases\\Images\\nena.jpeg");
 		im.show();
 		
-		ComputeHistogram ch= new ComputeHistogram();
+		MarcoBlanco ch= new MarcoBlanco();
 		ch.setup(null,im);
 		
 		ImageProcessor ip= im.getProcessor();
 		
 		ch.run(ip);
-
+    
 	}
 
 }
+ 
